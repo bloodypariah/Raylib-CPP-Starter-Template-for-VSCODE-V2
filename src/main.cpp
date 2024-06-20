1c5c6ffc40dd30a2a6ffe4e6c36db9ee84deab9e
Tads Coran <paulcoran0209@gmail.com>
2024-06-21 01:45:37 +0800

    Game of Life Simulator
    Command Keys
    - "Space bar"will stop the simulation.
    - "Return Key" will start/continue the simulation.
    - "R key" will create random spawning points for "live" cells.
    - "C key" will clear all the "live" cells.
    - You can use the mouse pointer to manually populate the grid with "live" cells

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,75 @@
 #include <raylib.h>
-#include "ball.h"
+#include <iostream>
+#include "simulation.hpp"
 
 int main()
 {
-    Color darkGreen = Color{20, 160, 133, 255};
+ Color GREY = {29, 29, 29, 255};
+ const int WINDOW_WIDTH = 1200;
+ const int WINDOW_HEIGHT = 800;  
+ const int CELL_SIZE = 6;
+ int FPS = 12;
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+ InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
+ SetTargetFPS(FPS);
+ Simulation simulation(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE);
 
-    Ball ball = Ball();
+ // Simulation Loop
+ while (WindowShouldClose() == false)
+ {
+     // 1. Event Handling
+     if(IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+     {
+        Vector2 mousePosition = GetMousePosition();
+        int row = mousePosition.y / CELL_SIZE;
+        int column = mousePosition.x /CELL_SIZE;
+        simulation.ToggleCell(row, column);
+     }
 
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
+     //Button Options
+     if(IsKeyPressed(KEY_ENTER))
+     {
+        simulation.Start();
+        SetWindowTitle("Game of Life is running...");
+     }
+     else if(IsKeyPressed(KEY_SPACE))
+     {
+        simulation.Stop();
+        SetWindowTitle("Game of Life has stopped.");
+     }
+     else if (IsKeyPressed(KEY_F))
+     {
+        FPS += 2;
+        SetTargetFPS(FPS);
+     }
+     else if (IsKeyPressed(KEY_S))
+     {
+        if(FPS > 5)
+        {
+            FPS -= 2;
+            SetTargetFPS(FPS);
+        }
+     }
+     else if (IsKeyPressed(KEY_R))
+     {
+        simulation.CreateRandomState();
+     }
+    else if (IsKeyPressed(KEY_C))
     {
-        BeginDrawing();
-        ClearBackground(darkGreen);
-        ball.Update();
-        ball.Draw();
-        EndDrawing();
+        simulation.ClearGrid();
     }
+    
+     
+      
+     
+     // 2. Updating State
+     simulation.Update();
+     // 3. Drawing
+     BeginDrawing();
+     ClearBackground(GREY);
+     simulation.Draw();
+     EndDrawing();
+}
 
-    CloseWindow();
-    return 0;
+ CloseWindow();
 }
